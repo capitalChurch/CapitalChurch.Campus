375144d8718047c631c1d370ff2d26196be20125
Kevyn Pinheiro Klava <kevynsax@gmail.com>
2019-09-22 07:56:49 -0300

    Correct packages reference

--- CapitalChurch.Campus.WebApi/Startup.cs
+++ CapitalChurch.Campus.WebApi/Startup.cs
@@ -1,14 +1,12 @@
 ﻿using CapitalChurch.Campus.WebApi.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
